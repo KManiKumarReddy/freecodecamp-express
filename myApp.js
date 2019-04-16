0bdc6b9e010ba0a867dfe9307e07e26add9bb394
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2019-04-16 17:55:53 +0530

    adding name get route for query params

--- myApp.js
+++ myApp.js
@@ -66,6 +66,7 @@ app.get("/:word/echo", (req, res) => {
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
+app.route("/name").get((req, res) => res.json({name: req.query.firstname + " " + req.query.lastname}));
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
